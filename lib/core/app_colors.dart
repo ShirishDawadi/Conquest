e41475e6a5345f4687efcdf1a0811237100d846f
Shirish Dawadi <shirishdawadi1@gmail.com>
2026-04-18 17:30:25 +0545

    feat: landing screen created and added fonts

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color greenish_1 = Color(0xFFDAF1DE);
+  static const Color greenish_2 = Color(0xFF8EB69B);
+  static const Color greenish_3 = Color(0xFF235347);
+  static const Color greenish_4 = Color(0xFF163832);
+  static const Color greenish_5 = Color(0xFF0B2B26);
+  static const Color greenish_6 = Color(0xFF051F20);
+}
---
+++ lib/landing_screen.dart
@@ -0,0 +1,93 @@
+import 'package:conquest/core/app_colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: -(screenWidth / 2),
+            child: SvgPicture.asset(
+              'assets/images/logo_screen.svg',
+              width: screenWidth * 2,
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  'conquest',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Vertigo',
+                    fontSize: 24,
+                    letterSpacing: 10,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  'Outside is the new meta',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Gpkn',
+                    fontSize: 10,
+                  ),
+                ),
+                const SizedBox(height: 40),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      side: BorderSide(color: AppColors.greenish_4),
+                      backgroundColor: AppColors.greenish_2,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text('Login'),
+                  ),
+                ),
+                const SizedBox(height: 15),
+
+                SizedBox(
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () {},
+                    style: OutlinedButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      side: BorderSide(color: AppColors.greenish_2),
+                      backgroundColor: AppColors.greenish_4,
+                      padding: const EdgeInsets.symmetric(vertical: 16),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                    ),
+                    child: const Text('Sign Up'),
+                  ),
+                ),
+                SizedBox(height: screenHeight * 0.1),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
